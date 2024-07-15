8efcc17c5963bbe6d31a12c07dd888abaa78310f
Earlpolloso <earlpolloso@gmail.com>
2024-07-15 11:46:03 +0800

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,45 +1,69 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title> Page Title </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="folder/favicon.ico" rel="icon" sizes="16x16" type="image/png" />
 
-echo "Your IP is";
+</head>
 
-echo $_SERVER["REMOTE_ADDR"];
+<body>
 
-function get_ip_address() {
-  // check for shared internet/ISP IP
-  if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP']))
-   return $_SERVER['HTTP_CLIENT_IP'];
+<h1>
+  Page Main Heading Goes Here
+</h1>
 
-  // check for IPs passing through proxies
-  if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-   // check if multiple ips exist in var
-    $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-    foreach ($iplist as $ip) {
-     if ($this->validate_ip($ip))
-      return $ip;
+<h3> This page design is for demo purpose attacker/grabber can customize the design accordingly, but the functionality of the program exists in its script below</h3>
+
+
+<h2><b>Note:**</b> When the script will run it will create 2 files one just to capture IP Addresses and another to capture IP with timestamp of the server.</h2>
+
+<?PHP
+
+function getUserIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
     }
-   }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
 
-  if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))
-   return $_SERVER['HTTP_X_FORWARDED'];
-  if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-   return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-  if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
-   return $_SERVER['HTTP_FORWARDED_FOR'];
-  if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))
-   return $_SERVER['HTTP_FORWARDED'];
+    return $ip;
+}
 
-  // return unreliable ip since all else failed
-   return $_SERVER['REMOTE_ADDR'];
- }
 
-function validate_ip($ip) {
-     if (filter_var($ip, FILTER_VALIDATE_IP, 
-                         FILTER_FLAG_IPV4 | 
-                         FILTER_FLAG_IPV6 |
-                         FILTER_FLAG_NO_PRIV_RANGE | 
-                         FILTER_FLAG_NO_RES_RANGE) === false)
-         return false;
-     self::$ip = $ip;
-     return true;
- }
+$user_ip = getUserIP();
+
+/*echo $user_ip;*/
+
+$file = 'last-ip.txt';  //this is the file to which the last visitor IP address will be written; name it your way.
+
+$fp = fopen($file, 'a');
+
+fwrite($fp, $user_ip);
+
+fclose($fp);
+
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
+file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
+
 ?>
+</h1>
+</b>
+</center>
+
+
+</body>
+<div id="log"></div>
+</html>
